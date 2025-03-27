5f9d04e09e65dc7fc4bc930a1e7a5e4c9c2c2911
Mike27-18 <mdnslugger@gmail.com>
2025-03-27 18:37:10 +0000

    Finiched Book class

--- Book.java
+++ Book.java
@@ -1,1 +1,9 @@
+public class Book extends LibrayItem{
+    private boolean isfiction;
+    private boolean fiction(String author, String title, String aquisitionDate, String callCode, double replacementCost, boolean checkedOut, boolean fiction){
+        super(author, title, aquisitionDate, callCode, replacementCost, checkedOut);
+        this.isfiction = fiction;
+    }
+}
+
 
