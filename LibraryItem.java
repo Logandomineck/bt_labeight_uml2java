69f94ec43130145cfe4bc4af19d023d1d1ac55c5
Mike27-18 <mdnslugger@gmail.com>
2025-03-27 18:52:49 +0000

    added constructor to LibraryItem class

--- LibraryItem.java
+++ LibraryItem.java
@@ -6,6 +6,16 @@ abstract class LibraryItem implements Borrowable {
     public String acquisitionDate;
     private boolean checkedOut;
 
+    public LibraryItem(String author, String title, String callCode, 
+    double replacementCost, String acquisitionDate, boolean checkedOut){
+      this.author = author;
+      this.title = title;
+      this.callCode = callCode;
+      this.replacementCost = replacementCost;
+      this.acquisitionDate = acquisitionDate;
+      this.checkedOut = checkedOut;
+    }
+
     @Override
     public boolean borrowItem(String date){
       return false;
