ade281a9f3939493559531ba6ec5547cb2c395ee
lostguardcs <emilymgosselin@gmail.com>
2025-03-27 18:55:00 +0000

    add Librarian class

--- Librarian.java
+++ Librarian.java
@@ -1,1 +1,10 @@
+public class Librarian extends Person {
+
+    private String mobilePhone;
+
+    public Librarian(String name, String id, String address, String mobilePhone){
+        super(name, id, address);
+        this.mobilePhone = mobilePhone;
+    }
+}
 
