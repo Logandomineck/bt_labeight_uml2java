526081523ef0c3bddafe69dc308492cae81b4434
lostguardcs <emilymgosselin@gmail.com>
2025-03-27 19:00:08 +0000

    added constructor

--- Person.java
+++ Person.java
@@ -2,4 +2,10 @@ abstract class Person {
     private String name;
     private String address;
     private String id;
+
+    public Person(String name, String address, String id){
+        this.name = name;
+        this.address = address;
+        this.id = id;
+    }
 }
